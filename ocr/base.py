0b58c26294b232e14f4b96ddfc749f35a12e87a1
Amit Timalsina <amittimalsina14@gmail.com>
2024-10-22 23:58:07 +0530

    Add IO readers

--- ocr/base.py
+++ ocr/base.py
@@ -1,32 +1,8 @@
 from abc import ABC, abstractmethod
-from pathlib import Path
 
-import cv2
 import numpy as np
 
-from ocr.exceptions.ocr_processing_error import ImageNotFoundError
 from ocr.schemas.ocr_result import OcrResult
-
-
-class ImageReader:
-    """Read image from various sources."""
-
-    @staticmethod
-    def read_image_from_path(image_path: str) -> np.ndarray:
-        """Read image from path."""
-        if not Path(image_path).is_file():
-            raise ImageNotFoundError
-        return cv2.imread(image_path)
-
-    @staticmethod
-    def read_image_from_bytes(image_bytes: bytes) -> np.ndarray:
-        """Read image from bytes."""
-        return cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
-
-    @staticmethod
-    def read_image_from_url(image_url: str) -> np.ndarray:
-        """Read image from url."""
-        raise NotImplementedError
 
 
 class OCRProvider(ABC):
