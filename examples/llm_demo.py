77f70d51c7d322defd46ac83cdff97cb04c6cd37
Amit Timalsina <amittimalsina14@gmail.com>
2024-12-07 12:28:23 +0530

    Remove typer cli, add support to predict directly from file

--- examples/llm_demo.py
+++ examples/llm_demo.py
@@ -11,10 +11,10 @@ Approaches to llm based extraction:
 from __future__ import annotations
 
 import asyncio
-from pathlib import Path  # noqa: TCH003
+from pathlib import Path
 
 import instructor
-import typer
+import pandas as pd
 from langsmith.wrappers import wrap_openai
 from openai import AsyncOpenAI
 
@@ -23,6 +23,7 @@ from document_classification.common.parsers.layout_preserving_formatter import (
     LayoutPreservingFormatter,
 )
 from document_classification.common.utils.json_to_ocr_text import json_to_ocr_text
+from document_classification.common.utils.parse_and_format import parse_and_format
 from document_classification.llm.build_classification_schema import (
     build_classification_schema,
 )
@@ -32,8 +33,8 @@ from document_classification.llm.evaluation import run_evaluation
 from document_classification.llm.prompt_technique import PromptTechnique
 from document_classification.llm.schemas.classification_entity import ClassificationEntity
 from document_classification.logger import logger
-
-app = typer.Typer()
+from document_classification.ocr.providers.closed_source.google_vision.ocr import GoogleVisionOCR
+from document_classification.ocr.readers.file_reader import FileReader
 
 DEFAULT_CLASSIFICATIONS = [
     ClassificationEntity(
@@ -60,7 +61,11 @@ def create_classifier(client: instructor.AsyncInstructor) -> OpenAILLMClassifier
     return OpenAILLMClassifier(client, llm_model="gpt-4o-2024-08-06")
 
 
-async def predict_document_type(file_path: Path, *, prompt_technique: PromptTechnique) -> None:
+async def predict_document_type_from_ocr_json(
+    ocr_json_file_path: Path,
+    *,
+    prompt_technique: PromptTechnique,
+) -> None:
     """Run document type prediction on a single file."""
     classification_schema = build_classification_schema(
         classifications=DEFAULT_CLASSIFICATIONS,
@@ -68,7 +73,29 @@ async def predict_document_type(file_path: Path, *, prompt_technique: PromptTech
     )
     parser = DefaultParser()
     formatter = LayoutPreservingFormatter()
-    ocr_text = json_to_ocr_text(file_path, parser, formatter)
+    ocr_text = json_to_ocr_text(ocr_json_file_path, parser, formatter)
+    classifier = create_classifier(create_openai_client())
+    classification_results = await classifier.classify_documents([ocr_text], classification_schema)
+    logger.info(f"Inference results: {classification_results}")
+
+
+async def predict_document_type(file_path: Path, *, prompt_technique: PromptTechnique) -> None:
+    """Run document type prediction on a single file."""
+    classification_schema = build_classification_schema(
+        classifications=DEFAULT_CLASSIFICATIONS,
+        prompt_technique=prompt_technique,
+    )
+    images = FileReader.read_file_from_path(file_path)
+    ocr_provider = GoogleVisionOCR()
+    ocr_dfs: list[pd.DataFrame] = []
+    for image in images:
+        ocr_result = ocr_provider.perform_ocr(image)
+        ocr_dfs.append(ocr_result.ocr_df)
+
+    ocr_df = pd.concat(ocr_dfs)
+    parser = DefaultParser()
+    formatter = LayoutPreservingFormatter()
+    ocr_text = parse_and_format(ocr_df, parser, formatter)
     classifier = create_classifier(create_openai_client())
     classification_results = await classifier.classify_documents([ocr_text], classification_schema)
     logger.info(f"Inference results: {classification_results}")
@@ -101,34 +128,21 @@ def log_evaluation_results(evaluation_results: dict) -> None:
             logger.info(f"  Classification: {result['classification']}")
 
 
-@app.command()
-def inference(
-    file_path: Path | None,
-    *,
-    prompt_technique: PromptTechnique = typer.Option(  # noqa: B008
-        default=PromptTechnique.ONE_SHOT,
-        help="Prompt technique to use",
-    ),
-) -> None:
-    """Run prediction on a single file."""
-    if file_path is None:
-        msg = "File path is required"
-        raise typer.BadParameter(msg)
-    asyncio.run(predict_document_type(file_path, prompt_technique=prompt_technique))
-
-
-@app.command()
-def evaluate(
-    directory: Path = OCR_JSON_DIRECTORY,
-    *,
-    prompt_technique: PromptTechnique = typer.Option(  # noqa: B008
-        default=PromptTechnique.ONE_SHOT,
-        help="Prompt technique to use",
-    ),
-) -> None:
-    """Run evaluation on all files in directory."""
-    asyncio.run(evaluate_documents(directory, prompt_technique=prompt_technique))
-
-
 if __name__ == "__main__":
-    app()
+    asyncio.run(
+        predict_document_type(
+            file_path=Path("files/e9e2ac9325664b4c9ca0324d5a5d782e.pdf"),
+            prompt_technique=PromptTechnique.COT,
+        ),
+    )
+
+    asyncio.run(
+        predict_document_type_from_ocr_json(
+            ocr_json_file_path=Path(
+                "ocr_jsons_tesseract/form_1040_schedule_c/0fe2cd862cd64e85af6f98464b69e57b3.json",
+            ),
+            prompt_technique=PromptTechnique.COT,
+        ),
+    )
+
+    asyncio.run(evaluate_documents(prompt_technique=PromptTechnique.COT))
