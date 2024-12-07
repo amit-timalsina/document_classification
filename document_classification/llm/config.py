b675b7ac00ecf4853816353f4fc44843597e683b
Amit Timalsina <amittimalsina14@gmail.com>
2024-12-07 22:11:26 +0530

    Update llmconfig data structure

--- document_classification/llm/config.py
+++ document_classification/llm/config.py
@@ -9,3 +9,13 @@ OCR_JSON_DIRECTORY = Path("ocr_jsons_tesseract")
 
 class LLMConfig:
     """Configuration for the LLM package."""
+
+    model_name: str
+    temperature: float
+    max_tokens: int
+    logprobs: bool
+    top_p: float
+    frequency_penalty: float
+    presence_penalty: float
+    top_logprobs: int
+    json_mode: bool
